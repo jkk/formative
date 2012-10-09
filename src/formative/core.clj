022e9df5114c88620452d8b3f4f1ab131b0606ff
Justin Kramer <jkkramer@gmail.com>
2012-10-09 13:34:19 -0400

    allow input parsing to emit detectable errors; parse-input method for :file type

--- src/formative/core.clj
+++ src/formative/core.clj
@@ -119,29 +119,51 @@
 
 ;;;;
 
-(defmulti parse-input (fn [spec s]
+(defn throw-problem
+  ([spec value]
+    (throw-problem spec value "%s is not valid"))
+  ([spec value msg]
+    (throw (ex-info (format msg (name (:name spec)))
+                    {:value value
+                     :problems [{:field-name (:name spec)
+                                 :spec spec
+                                 :msg msg}]}))))
+
+(defmulti parse-input (fn [spec v]
                         (:datatype spec (:type spec))))
 
-(defmethod parse-input :default [_ s]
-  s)
+(defmethod parse-input :default [_ v]
+  v)
 
-(defmethod parse-input :int [_ s]
-  (parse-long s))
+(defmethod parse-input :int [_ v]
+  (parse-long v))
 
-(defmethod parse-input :ints [_ s]
-  (map parse-long s))
+(defmethod parse-input :ints [_ v]
+  (map parse-long v))
 
-(defmethod parse-input :long [_ s]
-  (parse-long s))
+(defmethod parse-input :long [_ v]
+  (parse-long v))
 
-(defmethod parse-input :longs [_ s]
-  (map parse-long s))
+(defmethod parse-input :longs [_ v]
+  (map parse-long v))
 
-(defmethod parse-input :boolean [_ s]
-  (Boolean/valueOf s))
+(defmethod parse-input :boolean [_ v]
+  (Boolean/valueOf v))
 
-(defmethod parse-input :date [_ s]
-  (.parse (java.text.SimpleDateFormat. "yyyy-MM-dd") s))
+(defmethod parse-input :date [spec v]
+  (when-not (string/blank? v)
+    (try
+      (.parse (java.text.SimpleDateFormat. "yyyy-MM-dd") v)
+      (catch Exception e
+        (throw-problem spec v "%s is not a valid date")))))
+
+(defmethod parse-input :file [spec v]
+  (when-not (:upload-handler spec)
+    (throw (IllegalStateException.
+             (str "Missing :upload-handler for " (:name spec)))))
+  (if (string/blank? (:filename v))
+    ::absent
+    ((:upload-handler spec) spec v)))
 
 ;; TODO: more types
 
@@ -154,6 +176,7 @@
     (rest input)
     input))
 
+;; TODO: doc
 (defn parse-params [fields params]
   (let [;; FIXME: Should probably not rely on a private Ring fn (shhh)
         input (#'np/nest-params params
@@ -162,9 +185,24 @@
       (fn [row [fname spec]]
         (if (or (contains? input (name fname))
                 (contains? input fname))
-          (assoc row
-                 fname (parse-input spec (fix-input
-                                           (get-param input fname) spec)))
+          (let [raw-val (fix-input
+                          (get-param input fname) spec)
+                spec* (assoc spec :name fname)
+                val (parse-input spec* raw-val)]
+            (if (= val ::absent)
+              row
+              (assoc row fname val)))
           row))
       {}
       (partition 2 fields))))
+
+;;;;
+
+(defmacro with-fallback
+  [req form-fn & body]
+  `(try
+     ~@body
+     (catch clojure.lang.ExceptionInfo e#
+       (if-let [problems# (:problems (ex-data e#))]
+         (~form-fn ~req :problems problems#)
+         (throw e#)))))

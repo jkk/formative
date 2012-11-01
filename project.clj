fa2f2b959eaa12aab4064bac5bdd23be1aa30268
Justin Kramer <jkkramer@gmail.com>
2012-11-01 13:40:22 -0400

    bump ordered dep

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [hiccup "1.0.0"]
-                 [com.jkkramer/ordered "1.3.4"]
+                 [com.jkkramer/ordered "1.3.5"]
                  [ring/ring-core "1.1.1"]
                  [sundry "0.1.0-SNAPSHOT"]])

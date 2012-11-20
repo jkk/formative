0e51453fa691e20b542e0d99d7df94733fff0dbf
Justin Kramer <jkkramer@gmail.com>
2012-11-20 14:46:29 -0500

    project description & url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject formative "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Generates Hiccup forms and parses submitted form data"
+  :url "https://github.com/jkk/formative"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]

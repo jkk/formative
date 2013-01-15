cc3819bbb3a3cd6569c1d3d4e41fddcbf03548c9
Justin Kramer <jkkramer@gmail.com>
2013-01-15 13:14:36 -0500

    bump hiccup and ring deps

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hiccup "1.0.0"]
-                 [ring/ring-core "1.1.6"]
+                 [hiccup "1.0.2"]
+                 [ring/ring-core "1.1.7"]
                  [jkkramer/verily "0.4.1"]])

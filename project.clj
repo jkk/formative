44096f16aa6522a1904cba195c0f31cd30687777
Justin Kramer <jkkramer@gmail.com>
2013-06-27 13:54:09 -0400

    0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject formative "0.3.3-SNAPSHOT"
+(defproject formative "0.3.3"
   :description "Web forms - rendering, parsing, and validating"
   :url "https://github.com/jkk/formative"
   :license {:name "Eclipse Public License"

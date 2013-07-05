a645c040c78e27c4dc0d7eb16526dfc663184d36
Justin Kramer <jkkramer@gmail.com>
2013-07-05 08:17:13 -0400

    clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jkk/formative"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [hiccup "1.0.2"]
                  [ring/ring-core "1.2.0-RC1"]
                  [jkkramer/verily "0.4.4"]

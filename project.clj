12fda91fb6e542babc739a7e09f7e0472269047d
Justin Kramer <jkkramer@gmail.com>
2013-07-19 12:26:12 -0400

    bump verily dependency

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.2.0-RC1"]
-                 [jkkramer/verily "0.4.4"]
+                 [jkkramer/verily "0.5.0"]
                  [clj-time "0.5.1"]])

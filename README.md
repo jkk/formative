d938f10d71e0ff2ebd0e4c851d1393976095f331
Justin Kramer <jkkramer@gmail.com>
2013-07-19 17:31:15 -0400

    0.6.2

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ See a live demo at [formative-demo.herokuapp.com](http://formative-demo.herokuap
 Leiningen coordinate:
 
 ```clj
-[formative "0.6.1"]
+[formative "0.6.2"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject formative "0.6.1"
+(defproject formative "0.6.2"
   :description "Web forms - rendering, parsing, and validating"
   :url "https://github.com/jkk/formative"
   :license {:name "Eclipse Public License"

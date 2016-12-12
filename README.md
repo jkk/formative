cbb0fb0dc211bd0d1fa8b2e0b8fccc2ea6bbd374
iku000888 <kanumaiku@gmail.com>
2016-12-13 08:39:12 +0900

    Set up continuous integration
    This commit does the following:
    1. add a circle.yml file (circle ci was easier to set up)
    2. port the tests to cljc (originally in cljx)
    3. 2 implies that the minimum clojure version of this library becomes 1.7

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Formative
 
+![CircleCI] (https://circleci.com/gh/jkk/formative/tree/master.png?circle-token=a5b99afcb3c404441a1fbd41f6c0911f19f1e31b)
+
 Web forms for __Clojure__ and __ClojureScript__:
 
 * Render a form via pluggable renderers (comes with Bootstrap and other renderers built in)
--- project.clj
+++ project.clj
@@ -1,25 +1,19 @@
-(defproject formative "0.8.8"
+(defproject formative "0.8.9-SNAPSHOT"
   :description "Web forms - rendering, parsing, and validating"
   :url "https://github.com/jkk/formative"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [jkkramer/verily "0.6.0"]
                  [clj-time "0.5.1"]
                  [prismatic/dommy "0.1.1"]
                  [ring-anti-forgery "0.2.1"]]
-  :test-paths ["target/test-classes"]
+  :test-paths ["test"]
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/classes"
                    :rules :clj}
                   {:source-paths ["src"]
                    :output-path "target/classes"
-                   :rules :cljs}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
-                   :rules :clj}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
                    :rules :cljs}]}
   :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]
                                   [org.clojure/clojurescript "0.0-2138"]

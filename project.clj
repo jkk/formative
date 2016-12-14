a7ec8f91c28e7672ae5a9bad92dd5539600cea7a
iku000888 <kanumaiku@gmail.com>
2016-12-14 09:27:43 +0900

    Drop cljx support from project.clj

--- project.clj
+++ project.clj
@@ -9,17 +9,6 @@
                  [prismatic/dommy "0.1.1"]
                  [ring-anti-forgery "0.2.1"]]
   :test-paths ["test"]
-  :cljx {:builds [{:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :clj}
-                  {:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :cljs}]}
   :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]
                                   [org.clojure/clojurescript "0.0-2138"]
-                                  [com.cemerick/piggieback "0.0.5"]
-                                  [com.keminglabs/cljx "0.3.0"]]
-                   :plugins [[com.keminglabs/cljx "0.3.0"]]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
-                                                     cljx.repl-middleware/wrap-cljx]}
-                   :hooks [cljx.hooks]}})
+                                  [com.cemerick/piggieback "0.0.5"]]}})

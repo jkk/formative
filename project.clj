63c30b0a29311a4bad8a9aae5680133da1e872dd
iku000888 <kanumaiku@gmail.com>
2016-12-16 08:17:09 +0900

    0.8.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject formative "0.8.9-SNAPSHOT"
+(defproject formative "0.8.9"
   :description "Web forms - rendering, parsing, and validating"
   :url "https://github.com/jkk/formative"
   :license {:name "Eclipse Public License"
